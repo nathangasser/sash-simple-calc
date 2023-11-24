d1011c22f623bffa14f31a3f6edf00130be37355
Nate G <nathangasser@gmail.com>
2023-11-23 20:43:04 -0800

    Bushing branch merged

--- src/components/App copy.js
+++ src/components/App copy.js
@@ -1,48 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+
     const [sash, setSash] = useState({
         weight: '',
         position: 'top',
         output: ''
     });
 
-    const [useBushings, setUseBushings] = useState(false);
-
     const setOutput = (value = sash.weight, position = sash.position) => {
-        const sashWeight = parseFloat(value);
-        if (!isNaN(sashWeight)) {
+        const sashweight = parseFloat(value);
+        if (!isNaN(sashweight)) {
             let upperRange = 0;
             let lowerRange = 0;
-
-            // Adjust values based on whether the checkbox is checked
-            const reductionFactor = useBushings ? 0.6 : 1;
-
             if (position === 'bottom') {
-                upperRange = sashWeight / 2 * reductionFactor + 0.5;
-                lowerRange = sashWeight / 2 * reductionFactor;
+                upperRange = sashweight / 2 + .5;
+                lowerRange = sashweight / 2;
             } else if (position === 'top') {
-                upperRange = sashWeight / 2 * reductionFactor;
-                lowerRange = sashWeight / 2 * reductionFactor - 0.5;
+                upperRange = sashweight / 2;
+                lowerRange = sashweight / 2 - .5;
             } else {
-                setSash({ ...sash, output: 'Something weird is happening' });
-                return;
+                setSash({ ...sash, output: 'Something weird is happening'});
             }
-
-            const output = `${(lowerRange.toFixed(2) * 1).toString()} - ${(upperRange.toFixed(2) * 1).toString()}`;
-            setSash({ ...sash, weight: value, position: position, output: output });
+            const output = `${lowerRange.toFixed(2).toString()} - ${upperRange.toFixed(2).toString()}`;
+            setSash({...sash, weight: value, position: position, output: output});
         } else {
-            setSash({ ...sash, weight: value, position: position, output: 'Please enter a sash weight' });
+            setSash({...sash, weight: value, position: position, output: 'Please enter a sash weight'});
         }
-    };
-
-    useEffect(() => {
-        // Use useEffect to trigger setOutput after the state is updated
-        setOutput(sash.weight, sash.position);
-    }, [useBushings, sash.weight, sash.position]);
-
-    const handleCheckboxChange = () => {
-        setUseBushings(!useBushings);
     };
 
     return (
@@ -51,6 +35,7 @@ const App = () => {
                 <h1 className="display-6 fw-bold lh-1">Counterweight Calculator</h1>
                 <form>
                     <div className="d-grid mb-3 d-sm-flex">
+                        
                         <input
                             type="text"
                             className="form-control"
@@ -61,6 +46,7 @@ const App = () => {
                                 setOutput(e.target.value);
                             }}
                         />
+                        
                     </div>
                     <div className="form-check form-check-inline">
                         <input
@@ -73,11 +59,11 @@ const App = () => {
                                 setOutput(sash.weight, e.target.name);
                             }}
                         />
-                        <label className="form-check-label" htmlFor="flexRadioDefault1">
-                            Lower Sash
-                        </label>
-                    </div>
-                    <div className="form-check form-check-inline">
+                            <label className="form-check-label" htmlFor="flexRadioDefault1">
+                                Lower Sash
+                            </label>
+                        </div>
+                        <div className="form-check form-check-inline">
                         <input
                             className="form-check-input"
                             type="radio"
@@ -88,21 +74,9 @@ const App = () => {
                                 setOutput(sash.weight, e.target.name);
                             }}
                         />
-                        <label className="form-check-label" htmlFor="flexRadioDefault2">
-                            Upper Sash
-                        </label>
-                    </div>
-                    <div className="form-check my-3">
-                        <input
-                            className="form-check-input"
-                            type="checkbox"
-                            id="useBushings"
-                            checked={useBushings}
-                            onChange={handleCheckboxChange}
-                        />
-                        <label className="form-check-label" htmlFor="useBushings">
-                            Bushings 60%
-                        </label>
+                            <label className="form-check-label" htmlFor="flexRadioDefault2">
+                                Upper Sash
+                            </label>
                     </div>
                     <div className="d-grid d-md-flex my-3">
                         <h1>{sash.output === '' ? 'Please enter a sash weight' : sash.output}</h1>
